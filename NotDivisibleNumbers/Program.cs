31492e6e02148f16824b007093820c72f5a8f0fc
Mopcho <valerkatapoower@abv.bg>
2019-02-06 23:11:36 +0200

    Adding two new programs

---
+++ NotDivisibleNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotDivisibleNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 7 == 0)
+                {
+                    
+                }
+                else
+                {
+
+                    Console.Write("{0} ", i);
+
+                }
+            }
+        }
+    }
+}

568be6ea90a144145865b0fb2a3a1cc37be4ea6c
Mopcho <valerkatapoower@abv.bg>
2019-02-17 13:43:22 +0200

    Sort Three Numbers

--- SortThreeNumbers/Program.cs
+++ SortThreeNumbers/Program.cs
@@ -10,6 +10,43 @@ namespace SortThreeNumbers
     {
         static void Main(string[] args)
         {
+            int Num1 = int.Parse(Console.ReadLine());
+            int Num2 = int.Parse(Console.ReadLine());
+            int Num3 = int.Parse(Console.ReadLine());
+            if (Num1>Num2 && Num1>Num3)
+            {
+                if (Num2>Num3)
+                {
+                    Console.WriteLine("{0} {1} {2}", Num1, Num2, Num3);
+                }
+                else if (Num3>Num2)
+                {
+                    Console.WriteLine("{0} {1} {2}", Num1, Num3, Num2);
+                }
+            }
+            else if (Num2>Num1 && Num2>Num3)
+            {
+                if (Num1 > Num3)
+                {
+                    Console.WriteLine("{0} {1} {2}", Num2, Num1, Num3);
+                }
+                else if (Num1 < Num3)
+                {
+                    Console.WriteLine("{0} {1} {2}", Num2, Num3, Num1);
+                }
+
+            }
+            else if (Num3>Num2 && Num3>Num1)
+            {
+                if (Num2   > Num1)
+                {
+                    Console.WriteLine("{0} {1} {2}", Num3, Num2, Num1);
+                }
+                else if (Num1 > Num2)
+                {
+                    Console.WriteLine("{0} {1} {2}", Num3, Num1, Num2);
+                }
+            }
         }
     }
 }

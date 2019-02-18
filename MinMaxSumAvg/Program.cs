2e0548f4d891d515da079ed2eaec40866de5ca56
Mopcho <valerkatapoower@abv.bg>
2019-02-18 23:28:28 +0200

    MinMaxSumAvr

---
+++ MinMaxSumAvg/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxSumAvg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double min = 1000000;
+            double max = -100000;
+            double sum = 0;
+            double avr = 0;
+            for (int i=0;i<n;i++)
+            {
+                double Num = double.Parse(Console.ReadLine());
+                if (Num<min)
+                {
+                    min = Num;
+                }
+                if (Num>max)
+                {
+                    max = Num;
+                }
+                sum = Num + sum;
+                
+            }
+            avr = sum / n;
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+            Console.WriteLine(sum);
+            Console.WriteLine(avr);
+        }
+    }
+}

ead0b84684ab282a2ae4e607bfd3030464203f5b
Mopcho <valerkatapoower@abv.bg>
2019-02-21 23:48:51 +0200

    Idk anymore .i.

---
+++ CalculateAgain/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 1;
+            for (int i=1;i<=n;i++) 
+            {
+                counter = i;
+                for (int a=1;a<=n;a++,counter++)
+                {
+                    Console.Write(counter);
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}

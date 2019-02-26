36b4aa5a77acdb487f596d49c31fc4686d34af02
Mopcho <valerkatapoower@abv.bg>
2019-02-26 22:16:51 +0200

    Array

---
+++ AllocateArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllocateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] Array = new int[n];
+            for (int i=0;i<n;i++)
+            {
+                Array[i] = i * n;
+            }
+            for (int i=0;i<n;i++)
+            {
+                Console.WriteLine("{0}", Array[i]);
+            }
+        }
+    }
+}
---
+++ SpiralMatrix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiralMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mn = int.Parse(Console.ReadLine());
+            int y = 1;
+            int x = 1;
+            int number = 1;
+            int fakeA = 0;
+            for (int i=0;i<mn;i++,y++)
+            {
+                
+                for (int a=0;a<mn;a++,number++,fakeA++,x++)
+                {
+                    Console.Write("{0} ", number);
+                    
+
+                }
+                number = 1;
+                number = ((number + (mn - y)) + mn) + (mn - y);
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}

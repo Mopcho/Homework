667d9536036997a970300bfb6e8b2949c5d17c59
Mopcho <valerkatapoower@abv.bg>
2019-03-01 23:51:47 +0200

    Array tasks

---
+++ BIggestIncreasingSeq/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIggestIncreasingSeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] Arr = new int[n];
+            int c = 0;
+            int[] Arr1 = new int[n];
+            int counter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 1; i < n; i++, counter++)
+            {
+
+                if (Arr[i] > Arr[i - 1])
+                {
+                    c++;
+                }
+                else
+                {
+                    c = 0;
+                }
+                Arr1[counter] = c;
+            }
+            int biggest = 0;
+            for (int a = 0; a < n; a++)
+            {
+                if (Arr1[a] > biggest)
+                {
+                    biggest = Arr1[a];
+                }
+            }
+            Console.WriteLine(biggest + 1);
+        }
+    }
+}
---
+++ MaximalKSum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximalKSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] ArrN = new int[n];
+            int[] ArrK = new int[k];
+            int bigger = -100;
+            int counter = 1;
+            int fakeA = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ArrN[i] = int.Parse(Console.ReadLine());
+            }
+          
+            for (int i=0;i<k;i++)
+            {
+                for (int a = 0; a < n; a++)
+                {
+                   
+                    if (ArrN[a]>bigger)
+                    {
+                        
+                        bigger = ArrN[a];
+
+
+                        fakeA = a;
+                    }
+                   
+                }
+                Console.WriteLine("            {0}", bigger);
+                ArrN[fakeA] = -1000;
+              
+                ArrK[i] = bigger;
+                bigger = -100;
+            }
+            int sum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                sum = sum + ArrK[i];  
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

3f85f19af9b69d02b35fad312755d6472c840a7a
Mopcho <valerkatapoower@abv.bg>
2019-03-07 20:16:11 +0200

    ReverseArray

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] Arr = new int[n];
+            for (int i=0;i<n;i++)
+            {
+                Arr[i] = int.Parse(Console.ReadLine());
+            }
+            int[] RevArr = new int[n];
+            int upCounter = 0;
+            for (int i=n-1;i>=0;i--,upCounter++)
+            {
+                Console.WriteLine("            {0},{1}", upCounter, i);
+                RevArr[upCounter] = Arr[i];
+            }
+            for (int i=0;i<n;i++)
+            {
+                Console.WriteLine(RevArr[i]);
+            }
+        }
+    }
+}

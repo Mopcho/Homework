54adbf4e11cef0785996b9d0bcba4137ab567905
Mopcho <valerkatapoower@abv.bg>
2019-03-13 20:23:01 +0200

    TextInput

---
+++ TextInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int sum = 0;
+            for (int i=0;i<str.Length;i++)
+            {
+                sum = sum + (int)str[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

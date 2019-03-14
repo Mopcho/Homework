241c6ddfaa1a956cac544109693c75f1b95f9fcd
Mopcho <valerkatapoower@abv.bg>
2019-03-14 20:59:39 +0200

    BinarySearchMethod

---
+++ BinarySearchMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Arr = new int[2];
+            for (int i=0;i<Arr.Length;i++)
+            {
+                
+                    Arr[i] = int.Parse(Console.ReadLine());
+             
+            }
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Array.BinarySearch(Arr, n));
+        }
+    }
+}

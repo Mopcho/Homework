84cbd0e9c7ced87a0a223503e5e3520a05ac64cb
Mopcho <valerkatapoower@abv.bg>
2019-03-18 22:50:34 +0200

    FillTheMatrix

---
+++ FillTheMatrix/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillTheMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string letter = Console.ReadLine();
+            int[,] Arr = new int[n, n];
+            int c = 1;
+            if (letter == "a")
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int a = 0; a < n; a++)
+                    {
+                        Arr[a, i] = c;
+                        c++;
+                    }
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    for (int a = 0; a < n; a++)
+                    {
+                        Console.Write("{0} ", Arr[i, a]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else if (letter=="b")
+            {
+               int counter = 2;
+                for (int i = 0; i < n; i++,counter++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        for (int a = 0; a < n; a++)
+                        {
+                            Arr[a, i] = c;
+                            c++;
+                        }
+                    }
+                    else
+                    {
+                        for (int a=n-1;a>=0;a--)
+                        {
+                            Arr[a, i] = c;
+                            c++;
+                        }
+                    }
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    for (int a = 0; a < n; a++)
+                    {
+                        Console.Write("{0} ", Arr[i, a]);
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}

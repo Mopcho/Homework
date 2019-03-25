51ed5df50cb26130e65557f49fa9b837e828d872
Mopcho <valerkatapoower@abv.bg>
2019-03-25 22:50:49 +0200

    .i

---
+++ SortByStringLength/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortByStringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] Arr = new string[n];
+            int[] stringLengths = new int[n];
+            string[] sortedArr = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Arr[i] = Console.ReadLine();
+            }
+        
+         
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(sortedArr[i]);
+            }
+        }
+    }
+}

472c1520f229aa399a47529d3b6ee40e24be6715
Mopcho <valerkatapoower@abv.bg>
2019-03-26 22:58:25 +0200

    SayHelloMethod

---
+++ SayHello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHello
+{
+    class Program
+    {
+        public static void PrintName(string name)
+        {
+            Console.WriteLine("Hello, {0}", name);
+        }
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            PrintName(name);
+        }
+    }
+}

e0c7e6ab7c6f58bad44ff070a3fba9a99d75d118
Mopcho <valerkatapoower@abv.bg>
2019-03-27 23:48:45 +0200

    GetMaxMethod

---
+++ GetLargestNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetLargestNumber
+{
+    class Program
+    {
+        public static void GetMax(int a,int b,int c)
+        {
+       if (a>b && a>c)
+            {
+                Console.WriteLine(a);
+            }
+       else if (b>a && b>c)
+            {
+                Console.WriteLine(b);
+            }
+       else if (c>a && c>b)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            GetMax(a,b,c);
+        }
+    }
+}

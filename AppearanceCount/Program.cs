d915731872525b356698cfbc29395b553cbb5609
Mopcho <valerkatapoower@abv.bg>
2019-03-29 00:49:50 +0200

    EnglishDigitAndApperanceCount

---
+++ AppearanceCount/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppearanceCount
+{
+    class Program
+    {
+      public  static int[] FillArray(int n, int[] Arr)
+        {
+            Arr = new int[n];
+            for (int i=0;i<n;i++)
+            {
+                Arr[i] = int.Parse(Console.ReadLine());
+            }
+            return Arr;
+        }
+        public static int CountNumber(int n,int number,int[] Arr)
+        {
+            int counter = 0;
+            for (int i=0;i<n;i++)
+            {
+                if (Arr[i]==number)
+                {
+                    counter++;
+                   
+                }
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[] Arr = new int[n];
+            int number = int.Parse(Console.ReadLine());
+            Arr=FillArray(n,Arr);
+            
+            Console.WriteLine(CountNumber(n,number,Arr));
+
+        }
+    }
+}
---
+++ EnglishDigit/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishDigit
+{
+    class Program
+    {
+        public static int GetLastDigit(int number)
+        {
+            int x = number % 10;
+
+            return x;
+        }
+        public static string StringOutput(string numStr) {
+            string x="";
+            switch (numStr)
+            {
+                case "0":
+                    x = "zero";
+                    break;
+                case "1":
+                     x = "one";
+                    break;
+                case "2":
+                     x = "two";
+                    break;
+                case "3":
+                     x = "three";
+                    break;
+                case "4":
+                     x = "four";
+                    break;
+                case "5":
+                   x = "five";
+                    break;
+                case "6":
+                    x = "six";
+                    break;
+                case "7":
+                    x = "seven";
+                    break;
+                case "8":
+                    x = "eight";
+                    break;
+                case "9":
+                    x = "nine";
+                    break;
+            }
+            return x;
+            }
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string numStr = GetLastDigit(number).ToString();
+            Console.WriteLine(StringOutput(numStr));
+        }
+    }
+}

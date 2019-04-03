e82e9a28d632f6cd30f19308e59fa9c4f727c006
Mopcho <valerkatapoower@abv.bg>
2019-04-03 22:07:12 +0300

    Integral Calculation and NFactorial

---
+++ IntegerCalculations/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerCalculations
+{
+    class Program
+    {
+        public static void Minimum(int[] Arr)
+        {
+            int minimum = 1000;
+            for (int i = 0; i < 5; i++)
+            {
+                if (minimum>Arr[i])
+                {
+                    minimum = Arr[i];
+                }
+            }
+            Console.WriteLine(minimum);
+        }
+        public static void Maximum(int[] Arr)
+        {
+            int maximum = -1000;
+            for (int i = 0; i < 5; i++)
+            {
+                if (maximum < Arr[i])
+                {
+                    maximum = Arr[i];
+                }
+            }
+            Console.WriteLine(maximum);
+        }
+        public static void Average(int[] Arr)
+        {
+            double average = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                average = average + Arr[i];
+            }
+            average = average / 5;
+            Console.WriteLine("{0:f2}",average);
+        }
+        public static void Sum(int[] Arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += Arr[i];
+            }
+            Console.WriteLine(sum);
+        }
+        public static void Product(int[] Arr)
+        {
+            int product = 1;
+            for (int i = 0; i < 5; i++)
+            {
+               
+                product *= Arr[i];
+            }
+            Console.WriteLine(product);
+        }
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[5];
+            for (int i=0;i<5;i++)
+            {
+                Arr[i] = int.Parse(Console.ReadLine());
+            }
+            Minimum(Arr);
+            Maximum(Arr);
+            Average(Arr);
+            Sum(Arr);
+            Product(Arr);
+
+       
+        }
+    }
+}
---
+++ NFactorial/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFactorial
+{
+    class Program
+    {
+        public static void PrintArr(int[] Arr,int n)
+        {
+            for (int i = 0; i < n;i++)
+            {
+                Console.Write("{0} ", Arr[i]); 
+            }
+        }
+        public static void Ascend (int[] Arr,int n)
+        {
+            int smallest = Arr[0];
+            int smallestIndex = 0;
+            int[] sortedArr = new int[n];
+            for (int i=0;i<n;i++)
+            {
+               for (int a=0;a<n;a++)
+                {
+                    if (Arr[a]<smallest)
+                    {
+                        smallestIndex = a; 
+                        smallest = Arr[a];
+                    }
+                }
+                sortedArr[i] = Arr[smallestIndex];
+                Arr[smallestIndex] = 1000;
+                smallest = 1000;
+            }
+            PrintArr(sortedArr,n);
+        }
+        public static void Descend (int[] Arr,int n)
+        {
+            int biggest = Arr[0];
+            int biggestIndex = 0;
+            int[] sortedArr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int a = 0; a < n; a++)
+                {
+                    if (Arr[a] > biggest)
+                    {
+                        biggestIndex = a;
+                        biggest = Arr[a];
+                    }
+                }
+                sortedArr[i] = Arr[biggestIndex];
+                Arr[biggestIndex] = -1000;
+                biggest = -1000; 
+            }
+            PrintArr(sortedArr, n);
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string answer = Console.ReadLine();
+            int[] Arr = new int[n];
+            for (int i = 0; i < n;i++)
+            {
+                Arr[i] = int.Parse(Console.ReadLine());
+            }
+            if (answer=="A")
+            {
+                Ascend(Arr,n);
+            }
+            else if (answer=="D")
+            {
+                Descend(Arr,n);
+            }
+            
+        }
+    }
+}

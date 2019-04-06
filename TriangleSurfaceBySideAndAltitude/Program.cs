71eaac52311f67b3c5384b0729a1304e5c659558
Mopcho <valerkatapoower@abv.bg>
2019-04-06 22:29:22 +0300

    New Traingle Class

---
+++ TriangleSurfaceBySideAndAltitude/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleSurfaceBySideAndAltitude
+{
+    class Program
+    {
+        class Triangle
+        {
+            public double surface(double side,double altitude ) {
+                double surface = (side * altitude)/2;
+                return surface;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Triangle tr = new Triangle();
+            double side = double.Parse(Console.ReadLine());
+            double altitude = double.Parse(Console.ReadLine());
+            Console.WriteLine(tr.surface(side,altitude));
+        }
+    }
+}

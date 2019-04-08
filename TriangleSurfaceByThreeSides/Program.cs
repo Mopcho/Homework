63be20f819791dbb6c262d01751e89e7665bb64b
Mopcho <valerkatapoower@abv.bg>
2019-04-08 23:40:01 +0300

    Triangle Class Homework

---
+++ TriangleSurfaceByThreeSides/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleSurfaceByThreeSides
+{
+    class Triangle
+    {
+        private double p;
+        public double surface(double a,double b,double c)
+        {
+            double S;
+            double p = (a + b + c) / 2;
+            S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return S; 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle tr = new Triangle();
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine(tr.surface(a, b, c));
+
+        }
+    }
+}
---
+++ TriangleSurfaceByTwoSidesAndAnAngle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleSurfaceByTwoSidesAndAnAngle
+{
+    class Triangle
+    {
+        private double p;
+        public double surface(double a, double b, double h)
+        {
+            double S;
+            S = (a * b * Math.Sin(toRadians(h))) / 2;
+            return S;
+        }
+        public double toRadians(double h)
+        {
+            return Math.PI * h / 180;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle tr = new Triangle();
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            Console.WriteLine(tr.surface(a, b, h));
+            
+        }
+    }
+}

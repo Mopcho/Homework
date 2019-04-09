abee1943b915cfe643900931cf9153d4f598ba41
Mopcho <valerkatapoower@abv.bg>
2019-04-09 23:08:06 +0300

    Sum integers

---
+++ SumIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumIntegers
+{
+    class MyMath
+    {
+        public int sum()
+        {
+            int sum = 0;
+            return sum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath mm = new MyMath();
+
+        }
+    }
+}

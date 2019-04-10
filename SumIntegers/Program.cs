18077b0373947d2644a8dd72497c5a9dc4eadaef
Mopcho <valerkatapoower@abv.bg>
2019-04-10 23:52:36 +0300

    Sum Integers fixed

--- SumIntegers/Program.cs
+++ SumIntegers/Program.cs
@@ -8,9 +8,44 @@ namespace SumIntegers
 {
     class MyMath
     {
-        public int sum()
+        public int sum(string strNumbers)
         {
             int sum = 0;
+            int numbers = 1;
+        for (int i=0;i<strNumbers.Length;i++)
+             {
+                if (strNumbers[i]==' ')
+                {
+                    numbers++;
+                }
+             }
+            int checkpoint = 0;
+            int[] Arr = new int[numbers];
+            int counter = 0;
+            for (int i=0;i<strNumbers.Length-1;i++)
+            {
+                if (strNumbers[i+1]==' ')
+                {
+                    switch (i-checkpoint)
+                    {
+                        case 0:
+                            Arr[counter] = int.Parse(strNumbers[i].ToString());
+                            break;
+                        case 1:
+                            Arr[counter] = (int.Parse(strNumbers[i - 1].ToString()) * 10) + int.Parse(strNumbers[i].ToString());
+                            break;
+                        case 2:
+                            Arr[counter] = (int.Parse(strNumbers[i - 2].ToString()) * 100) + (int.Parse(strNumbers[i - 1].ToString()) * 10) + int.Parse(strNumbers[i].ToString());
+                            break;
+                    }
+                    counter++;
+                    checkpoint = i + 2;
+                }
+            }
+            for (int i=0;i<numbers;i++)
+            {
+                sum += Arr[i];
+            }
             return sum;
         }
     }
@@ -19,7 +54,9 @@ namespace SumIntegers
         static void Main(string[] args)
         {
             MyMath mm = new MyMath();
-
+            string strNumbers = Console.ReadLine();
+            strNumbers += " ";
+            Console.WriteLine(mm.sum(strNumbers));
         }
     }
 }

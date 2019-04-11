a4b8bee45904ded457f877915e421c0207d795bf
Mopcho <valerkatapoower@abv.bg>
2019-04-11 22:43:10 +0300

    Inverted String

---
+++ ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string invertedStr = "";
+            for (int i=str.Length-1;i>=0;i--)
+            {
+                invertedStr += str[i];
+            }
+            Console.WriteLine(invertedStr);
+        }
+    }
+}

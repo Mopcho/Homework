fc64bbad5bc4ece57d6ffb2ab7f41c642446ca07
Mopcho <valerkatapoower@abv.bg>
2019-04-12 23:23:31 +0300

    Adding checking brackets and sub string

---
+++ CorrectBrackets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectBrackets
+{
+    class Program
+    {
+        static bool isCorrect(string str) {
+            bool isTrue = false;
+            int balancer = 0;
+            for (int i=0;i<str.Length;i++)
+            {
+                if (str[i]=='(')
+                {
+                    balancer++;
+                }
+                if (str[i]==')')
+                {
+                    balancer--;
+                }
+            }
+            if (balancer==0)
+            {
+                isTrue = true;
+            }
+            return isTrue;
+            }
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(isCorrect(str));
+        }
+    }
+}
---
+++ SubStringInText/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubStringInText
+{
+    class Program
+    {
+        public static int countWordInText(string text, string word)
+        {
+            int count = 0;
+            int wordLength = word.Length;
+            int textLength = text.Length;
+            string checker = "";
+            for (int i=0;i<textLength;i++)
+            {
+                if (textLength-i<=word.Length-1)
+                {
+
+                }
+                else
+                {
+                    for (int a=i;a<i+word.Length;a++)
+                    {
+                        checker += text[a];
+                    }
+                    if (checker==word)
+                    {
+                        count++;
+                    }
+                    checker = "";
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+            Console.WriteLine(countWordInText(text,word));
+        }
+    }
+}

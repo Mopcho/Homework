2dd96b293e2d8ce96cf62ecf21a3ae6fb5248cba
Mopcho <valerkatapoower@abv.bg>
2019-04-13 23:01:56 +0300

    String length

---
+++ StringLength/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string holder = "";
+            for (int i=0;i<input.Length;i++)
+            {
+                if (i>19)
+                {
+                    holder += "*";
+                }
+                else
+                {
+                    holder += input[i];
+                }
+               
+            }
+            Console.WriteLine(holder);
+
+        }
+    }
+}

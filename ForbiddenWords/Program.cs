3624ed60ef5448f80d092a48865a48b05ffc887e
Mopcho <valerkatapoower@abv.bg>
2019-04-16 23:55:56 +0300

    Forbidden Words part 1

---
+++ ForbiddenWords/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForbiddenWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentance = Console.ReadLine();
+            string words = Console.ReadLine();
+            string[] arrWords = words.Split(' ');
+            string[] arrSentance = sentance.Split(' ');
+            string holder = "";
+            for (int i=0;i<arrSentance.Length;i++)
+            {
+                for (int a = 0; a < arrWords.Length; a++) {
+                    if (arrSentance[i] ==arrWords[a])
+                    {
+                        for (int c = 0; c < arrSentance[i].Length; c++) {
+                            holder = holder +"*" ;
+                        }
+                        holder += " ";
+                    }
+                    else
+                    {
+                        holder += " "+arrSentance[i];
+                    }
+                        }
+            }
+            Console.WriteLine(holder);
+
+        }
+    }
+}

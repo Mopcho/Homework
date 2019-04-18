6a2f22017e00335be30948fc6e19f4b68ed5b218
Mopcho <valerkatapoower@abv.bg>
2019-04-18 23:36:14 +0300

    Unicode Characters

---
+++ UnicodeCharacters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string holder = "";
+            for (int i=0;i<word.Length;i++)
+            {
+                holder+="\\u00";
+                holder +=string.Format("{0:X}",(int)word[i]);
+            }
+            Console.WriteLine(holder);
+        }
+    }
+}

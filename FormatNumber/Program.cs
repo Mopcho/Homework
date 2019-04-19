c446ac4903787ab7152653b64e0e5df71abb4030
Mopcho <valerkatapoower@abv.bg>
2019-04-19 22:19:12 +0300

    Format Number

---
+++ FormatNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nUmBeR = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0:x}     {1:d}         {2}%         {3:E2}",nUmBeR,nUmBeR,nUmBeR,nUmBeR);
+        }
+    }
+}

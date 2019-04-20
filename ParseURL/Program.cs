823182a1fd720ddbdad9d9c6deb5bb043e5710ad
Mopcho <valerkatapoower@abv.bg>
2019-04-20 23:04:42 +0300

    Working With Strings.Adding 1 complete and 1 not complete tasks

---
+++ ParseURL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseURL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = Console.ReadLine();
+           
+            
+            
+        }
+    }
+}
---
+++ ReverseSentance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseSentance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentance = Console.ReadLine();
+            string[] words = sentance.Split(' ');
+            string holder = "";
+            for (int i=words.Length-1;i>=0;i--)
+            {
+                holder += " " + words[i];
+            }
+            
+            Console.WriteLine(holder);
+        }
+    }
+}

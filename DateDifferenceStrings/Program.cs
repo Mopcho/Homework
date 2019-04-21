baaf60527970cb1df14afb1c8e554b287f5f3e12
Mopcho <valerkatapoower@abv.bg>
2019-04-21 23:12:29 +0300

    Date Difference 1

---
+++ DateDifferenceStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateDifferenceStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date =DateTime.Parse(Console.ReadLine());
+            DateTime dateTwo = DateTime.Parse(Console.ReadLine());
+            double dateThree = (date - dateTwo).TotalDays;
+
+        }
+    }
+}

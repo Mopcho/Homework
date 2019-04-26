55fd8d9589b8f670702dcd54f85c71925ffa690c
Mopcho <valerkatapoower@abv.bg>
2019-04-26 22:35:09 +0300

    Encode and Decode

---
+++ EncodeAndDecode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodeAndDecode
+{
+    class Program
+    {
+        public static string Encode(string key,string text)
+        {
+          
+            string holder = "";
+            int counter = 0;
+       for (int i=0;i<text.Length;i++)
+            {
+                if (counter >= key.Length - 1)
+                {
+                    counter = 0;
+                }
+                holder +=  (char)(text[i] ^ key[i%key.Length]);
+                
+            
+            }
+            return holder;
+        }
+        public static string Decode(string key,string text)
+        {
+            return Encode(key,text);
+        }
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string key = "abc";
+            Console.WriteLine(Encode(key,text));
+            Console.WriteLine(Decode(key, Encode(key,text)));
+        }
+    }
+}

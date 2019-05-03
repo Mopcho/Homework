6db61dbd0396f405d443c9a4fde96b0ed3c42535
Mopcho <valerkatapoower@abv.bg>
2019-05-03 23:11:25 +0300

    MythicalNumbers

---
+++ MythicalNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythicalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string numStr = number.ToString();
+            int sum = 0;
+            if (numStr[2]=='0')
+            {
+                sum = int.Parse(numStr[0].ToString()) + int.Parse(numStr[1].ToString());
+            }
+            else if (int.Parse(numStr[2].ToString())<5)
+            {
+                sum = (int.Parse(numStr[0].ToString()) + int.Parse(numStr[1].ToString()))/int.Parse(numStr[2].ToString());
+            }
+            else if (int.Parse(numStr[2].ToString()) > 5)
+            {
+                sum = (int.Parse(numStr[0].ToString()) + int.Parse(numStr[1].ToString())) * int.Parse(numStr[2].ToString());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

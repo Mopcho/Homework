95964bbf7284c017a80873bb15c22ee5469023dc
Mopcho <valerkatapoower@abv.bg>
2019-05-04 23:32:30 +0300

    SquareRoot1

---
+++ SquareRoot1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            try
+            {
+                if (number < 0)
+                {
+                    throw new Exception();
+                }
+                double root = Math.Sqrt(number);
+                Console.WriteLine(root);
+              
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("Invalid Number!");
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye!");
+            }
+            
+        }
+    }
+}

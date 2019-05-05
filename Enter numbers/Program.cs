f23a94808a737f78852464613b1781d5fd320b6c
Mopcho <valerkatapoower@abv.bg>
2019-05-05 23:46:25 +0300

    Enter Numbers

---
+++ Enter numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enter_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ EnterNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string holder = "1<";
+            int[] Arr = new int[10];
+            Arr[0] = 1;
+            for (int i=1;i<Arr.Length;i++)
+            {
+                Arr[i] = int.Parse(Console.ReadLine());
+                if (Arr[i]<Arr[i-1] || Arr[i]>=100)
+                {
+                    throw new Exception("Exception");
+                }
+                holder += Arr[i].ToString()+"<";
+            }
+            holder +="100";
+            Console.WriteLine(holder);
+          
+        }
+    }
+}

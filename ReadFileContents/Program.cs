471d835c06e8167806416c56fd87100d9839c98e
Mopcho <valerkatapoower@abv.bg>
2019-05-06 23:30:13 +0300

    Read File Contents

---
+++ ReadFileContents/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFileContents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = Console.ReadLine();
+                Console.WriteLine(System.IO.File.ReadAllText(path));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid Directorty");
+            }
+        }
+    }
+}

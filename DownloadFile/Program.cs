a93a00677865978684b804c2300c5bf73968606c
Mopcho <valerkatapoower@abv.bg>
2019-05-07 22:22:54 +0300

    Download File

---
+++ DownloadFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = Console.ReadLine();
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadFile(url, @"C:\Test\image.png");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid Address");
+            }
+               
+        }
+    }
+}

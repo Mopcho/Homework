bea5fdd4f6616e5d77b5694261e7f83fcb67dfda
Mopcho <valerkatapoower@abv.bg>
2019-05-08 23:19:52 +0300

    Game Part1

---
+++ Game/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nick;
+            int age;
+            int height;
+            int muscle;
+            int stamina;
+        }
+    }
+}

52217b8c8decd591a2b3846c536fd6dda7c40ed1
Mopcho <valerkatapoower@abv.bg>
2019-05-13 22:53:42 +0300

    File Class Test

---
+++ FileClassTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Console.ReadLine();
+           string text= File.ReadAllText(file);
+            string[] arr = text.Split('\n');
+            File.WriteAllText(file,arr.Length.ToString());
+        }
+    }
+}

407d547316a6db2600c359b278af4fb677fd27b6
Mopcho <valerkatapoower@abv.bg>
2019-05-16 23:34:32 +0300

    Dictionary Test

---
+++ DictionaryTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var dictionary = new Dictionary<string, int>();
+            dictionary.Add("cat", 2);
+            dictionary.Add("dog", 1);
+            dictionary.Add("llama", 0);
+            dictionary.Add("iguana", -1);
+           
+            Console.WriteLine("DICTIONARY 1: " + dictionary.Count);
+
+            
+            var dictionary2 = new Dictionary<string, int>()
+        {
+            {"cat", 2},
+            {"dog", 1},
+            {"llama", 0},
+            {"iguana", -1}
+        };
+
+        }
+    }
+}

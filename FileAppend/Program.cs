a8030a2cf742eaa39cd6e456daf59015275b5528
Mopcho <valerkatapoower@abv.bg>
2019-05-17 23:53:54 +0300

    File Append

---
+++ FileAppend/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAppend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            string contentStr = Console.ReadLine();
+            string[] content = contentStr.Split(' ');
+            File.AppendAllLines(path, content);
+        }
+    }
+}

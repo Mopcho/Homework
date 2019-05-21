528ec13593d736d30228da66191216794671022c
Mopcho <valerkatapoower@abv.bg>
2019-05-21 22:32:04 +0300

    UrlParse (Stage 1)

---
+++ URLParse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace URLParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Console.ReadLine();
+            string holder = "";
+            using (StreamReader sr=new StreamReader(file))
+            {
+                holder+=sr.ReadToEnd();
+            }
+            string[] urls = holder.Split('\n');
+        }
+    }
+}

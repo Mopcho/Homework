6a90280bcd6b4f986c7d3735968cce9c3f22fc09
Mopcho <valerkatapoower@abv.bg>
2019-07-11 23:58:12 +0300

    Testing namespaces Part 3

---
+++ Animals/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.Grubnachni
+{
+    class Dog 
+    {
+        public int Legs { get; set; }
+        public int Eyes { get; set; }
+
+
+    }
+}

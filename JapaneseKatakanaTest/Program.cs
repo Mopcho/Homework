ec4331f6382111517d442df05ce9518094a5600d
Mopcho <valerkatapoower@abv.bg>
2019-08-01 23:38:15 +0300

    Adding Customizable Word Count

--- JapaneseKatakanaTest/Program.cs
+++ JapaneseKatakanaTest/Program.cs
@@ -12,15 +12,16 @@ namespace JapaneseKatakanaTest
     {
         public static string Romaji="a i u e o ka ki ku ke ko ga gi gu ge go sa shi su se so za ji zu ze zo ta chi tsu te to da ji zu de do na ni nu ne no ha hi fu he ho ba bi bu be bo pa pi pu pe po ma mi mu me mo ya yu yo ra ri ru re ro wa wo n";
         public static string[] MRomaji = Romaji.Split(' ');
-        public static void Test(int group)
+        public static void Test(int group,int wordCount)
         {
 
             Random rnd = new Random();
-            for (int i=0;i<25;i++)
+            for (int i=0;i<wordCount;i++)
             {
                 
                 Console.Write("{0} ",MRomaji[rnd.Next(0,group)]);
             }
+            Console.WriteLine();
         }
     }
   
@@ -39,7 +40,8 @@ namespace JapaneseKatakanaTest
             //68 = 9th group
             //70 = 10th group
             //71 = 11th group
-            RTK.Test(71);
+            int wordCount = int.Parse(Console.ReadLine());
+            RTK.Test(35,wordCount);
         }
     }
 }

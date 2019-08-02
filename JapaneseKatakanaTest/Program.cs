d64a1c249522eded5c9c81be2d33b05b61c52492
Mopcho <valerkatapoower@abv.bg>
2019-08-02 23:56:04 +0300

    Japanese Katakana Part 3

--- JapaneseKatakanaTest/Program.cs
+++ JapaneseKatakanaTest/Program.cs
@@ -7,21 +7,23 @@ using System.Threading.Tasks;
 
 namespace JapaneseKatakanaTest
 {
-   
+
     static class RTK
     {
-        public static string Romaji="a i u e o ka ki ku ke ko ga gi gu ge go sa shi su se so za ji zu ze zo ta chi tsu te to da ji zu de do na ni nu ne no ha hi fu he ho ba bi bu be bo pa pi pu pe po ma mi mu me mo ya yu yo ra ri ru re ro wa wo n";
-        public static string[] MRomaji = Romaji.Split(' ');
+
+        public static string[][] romaji =
+                 "a.i.u.e.o ka.ki.ku.ke.ko.ga.gi.gu.ge.go sa.shi.su.se.so.za.ji.zu.ze.zo ta.chi.tsu.te.to.da.ji.zu.de.do na.ni.nu.ne.no ha.hi.fu.he.ho.ba.bi.bu.be.bo.pa.pi.pu.pe.po ma.mi.mu.me.mo ya.yu.yo ra.ri.ru.re.ro wa.wo n"
+                     .Split(' ').Select(group => group.Split('.').ToArray()).ToArray();
         public static void Test(int group,int wordCount)
         {
-
-            Random rnd = new Random();
-            for (int i=0;i<wordCount;i++)
+          for (int i=0;i<group;i++)
             {
-                
-                Console.Write("{0} ",MRomaji[rnd.Next(0,group)]);
+                for (int a=0;romaji[i].Length;a++)
+                {
+
+                }
             }
-            Console.WriteLine();
+         
         }
     }
   
@@ -40,8 +42,9 @@ namespace JapaneseKatakanaTest
             //68 = 9th group
             //70 = 10th group
             //71 = 11th group
+            int groups =int.Parse(Console.ReadLine());
             int wordCount = int.Parse(Console.ReadLine());
-            RTK.Test(35,wordCount);
+            RTK.Test(groups,wordCount);
         }
     }
 }

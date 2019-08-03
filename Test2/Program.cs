4514ee8a94e112316d2b11767da639c295b8b15c
Mopcho <valerkatapoower@abv.bg>
2019-08-03 23:57:03 +0300

    Starting up on Tamagochi pet game

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -14,10 +14,26 @@ namespace Test2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What animal do you want to buy?");
+            string answer = Console.ReadLine();
+            if (answer == "Cat")
+            {
+                Cat cat = new Cat();
+            }
+            else if (answer == "Dog")
+            {
+                Console.WriteLine("Chose a name and an age of the dog");
+                Console.Write("Age: ");
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Dog dog = new Dog(age, name);
+            }
+            while (true)
+            {
 
-            Cat cat = new Cat();
-            Dog dog  = new Dog();
-
+            }
         }
         
     }

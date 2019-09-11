ac1cebb8d0c9d61683ac6ed4f845d515d1edc57a
Mopcho <valerkatapoower@abv.bg>
2019-09-11 22:30:21 +0300

    Testing WPF

---
+++ MyOwnProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Download().Wait();
+        }
+
+        public static async Task Download()
+        {
+            var webClient = new WebClient();
+            webClient.DownloadProgressChanged += ((o, e) =>
+            {
+                double currentMb = (double.Parse(e.BytesReceived.ToString()) / 1024) / 1024;
+                double mbToDownload = (double.Parse(e.TotalBytesToReceive.ToString()) / 1024) / 1024;
+                Console.WriteLine(e.ProgressPercentage + " " + currentMb + " MB / " + mbToDownload + " MB");
+
+            });
+            Console.WriteLine("Started Downloading");
+          
+            await webClient.DownloadFileTaskAsync(new Uri("https://oqbkip.oloadcdn.net/dl/l/BiswYu0mXuAuPcp2/8Cb0glIp_ig/Borat.2006.BDRip.XviD.AC3-WAR.mp4"), "./myFilm.mp4");
+        }
+    }
+}

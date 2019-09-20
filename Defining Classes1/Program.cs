4acaee802d0c095943378b900eb18053b055dadd
Mopcho <valerkatapoower@abv.bg>
2019-09-20 23:48:48 +0300

    Part 5

--- Defining Classes1/Program.cs
+++ Defining Classes1/Program.cs
@@ -12,9 +12,12 @@ namespace Defining_Classes1
     }
     public class Battery
     {
-        public int HoursIdle { get; set; }
-        public int HoursTalk { get; set; }
-        public BatteryType Model { get; set; }
+        public int HoursIdle { get { return _hoursIdle; } set { _hoursIdle = value; } }
+        public int HoursTalk { get {return _hourseTalk ;} set {_hourseTalk=value ;} }
+        public BatteryType Model { get {return _model; } set {_model=value; } }
+        private int _hoursIdle;
+        private int _hourseTalk;
+        private BatteryType _model;
         public Battery(int hoursIdle, int hoursTalk, BatteryType model)
         {
             this.HoursIdle = hoursIdle;
@@ -22,25 +25,37 @@ namespace Defining_Classes1
             this.Model = model;
         }
     }
+
+
     public class Display
     {
-        public int Size { get; set; }
-        public int NumberOfColors { get; set; }
+        public int Size { get { return _size; } set { _size = value; } }
+        public int NumberOfColors { get {return _numberOfColors; } set { _numberOfColors = value;  } }
+        private int _size;
+        private int _numberOfColors;
         public Display(int size, int numberOfColors)
         {
             this.Size = size;
             this.NumberOfColors = numberOfColors;
         }
     }
+
+
+
     public class Phone
     {
-        public string Model { get; set; }
-        public string Manufacturer { get; set; }
-        public double Price { get; set; }
-        public string Owner { get; set; }
-        public Battery Battery { get; set; }
-        public Display Display { get; set; }
-
+        public string Model { get {return _model; } set {  _model = value; } }
+        public string Manufacturer { get {return _manufacturer; } set {_manufacturer=value; } }
+        public double Price { get {return _price; } set {_price=value; } }
+        public string Owner { get {return _owner; } set {_owner=value; } }
+        public Battery Battery { get {return _battery; } set { _battery = value ; } }
+        public Display Display { get {return _display; } set {_display=value; } }
+        private string _model;
+        private string _manufacturer;
+        private double _price;
+        private string _owner;
+        private Battery _battery;
+        private Display _display;
         public Phone(string model, string manufacturer)
         {
             this.Model = model;
@@ -73,7 +88,9 @@ namespace Defining_Classes1
     {
         static void Main(string[] args)
         {
-           
+          
+
+            
         }
     }
 }

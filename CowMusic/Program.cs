e9566f5e73778398f4dd3bfc82a7e196d21984c0
Mopcho <valerkatapoower@abv.bg>
2019-10-06 23:52:28 +0300

    Scratching out the plan for the code

--- CowMusic/Program.cs
+++ CowMusic/Program.cs
@@ -1,19 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using System.Timers;
 
 namespace CowMusic
 {
     class Program
     {
-        public static void Main()
+        private static bool lever;
+
+        static void Main(string[] args)
         {
-           
+            var timer = new Timer(1000);
+            timer.AutoReset = true;
+
+            timer.Elapsed += EventHandler;
+
+            timer.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void EventHandler(object sender, ElapsedEventArgs e)
+        {
+
+            
+            int minute = DateTime.Now.Minute;
+          
+            int[] desiredMinutes = { 18, 20 };
+
+            for (int i = 0; i < desiredMinutes.Length; i++)
+            {
+                int desiredMinute = desiredMinutes[i];
+
+                if (desiredMinute == minute && !lever)
+                {
+                    lever = true;
+
+                    string urlForThePlayList =
+                        "https://www.youtube.com/watch?v=foE1mO2yM04&list=PLwLeqKCWCxyTQWisBC4oWfV77UaJ6WPdN&index=1";
+
+                    Process.Start(urlForThePlayList);
+                }
+                else if(lever && minute==desiredMinute+1)
+                {
+                    lever = false;
+                    
+                }
+            }
         }
     }
 }
-

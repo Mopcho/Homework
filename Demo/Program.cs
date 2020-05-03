190f0f2846ff2a59f92cb725d4662590feef8f46
Mopcho <valerkatapoower@abv.bg>
2020-05-03 23:25:38 +0300

    Something

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -6,12 +6,54 @@ using System.Threading.Tasks;
 
 namespace Demo
 {
+    class Person
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hello");
+        }
+        protected int _age;
+        public void SetAge(int age)
+        {
+            this._age = age;
+        }
+    }
+    class Studen : Person
+    {
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is {0}",_age);
+        }
+        public void GoToClassess()
+        {
+         
+            Console.WriteLine("Going to class");
+        }
+    }
+    class Teacher : Person
+    {
+        private string subject;
+        public void Explain()
+        {
+            Console.WriteLine("Explaining");
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        class StudentAndTeacherTest
         {
-            char c = '.';
-            Console.WriteLine((int)c);
+            static void Main(string[] args)
+            {
+                var myPerson = new Person();
+                myPerson.Greet();
+                var myStudent = new Studen();
+                myStudent.SetAge(21);
+                myStudent.Greet();
+                myStudent.ShowAge();
+                var myTeacher = new Teacher();
+                myTeacher.Greet();
+                myTeacher.Explain();
+            }
         }
     }
 }
---
+++ PhotoAlbumTarea/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumTarea
+{
+    class PhotoAlbum
+    {
+       protected int _numberOfPages;
+        public PhotoAlbum()
+        {
+            this._numberOfPages = 16;
+        }
+        public PhotoAlbum(int num)
+        {
+            this._numberOfPages = num;
+        }
+        public int GetNumberOfPages()
+        {
+            return this._numberOfPages;
+        }
+    }
+    class BigPhotoAlbum : PhotoAlbum
+    {
+        public BigPhotoAlbum()
+        {
+            this._numberOfPages = 64;
+        }
+    }
+    class AlbumTest
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                
+            }
+        }
+    }
+}

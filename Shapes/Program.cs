f550451f303c70816a2bf56eac69aee3a77909f6
Mopcho <valerkatapoower@abv.bg>
2020-05-04 23:02:12 +0300

    Adding shape calsses

---
+++ Shapes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Shape
+    {
+        protected Location c;
+
+        public string ToString()
+        {
+            return "";
+        }
+
+        public double Area()
+        {
+            return 0.000;
+        }
+
+        public double Perimeter()
+        {
+            return 0.000;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        protected double side1, side2;
+    }
+
+    class Circle : Shape
+    {
+        protected double radius;
+    }
+
+    class Location
+    {
+        private double x, y;
+    }
+
+    class TestShape
+    {
+        static void Main()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

a6c84122e93be41085e2a552505417e135cec926
Mopcho <valerkatapoower@abv.bg>
2020-05-25 21:54:34 +0300

    Students Groups

--- StudentGoups/Extensions.cs
+++ StudentGoups/Extensions.cs
@@ -6,14 +6,20 @@ using System.Threading.Tasks;
 
 namespace StudentGoups
 {
-   public static class Extensions
+    public static class Extensions
     {
-         public static IEnumerable<Student> ExtractWithTwoMarks(this List<Student> students)
-         {
+        public static IEnumerable<Student> ExtractWithTwoMarks(this List<Student> students)
+        {
             var sortedStuds = from student in students
                               where student.Marks.Count < 3
                               select student;
             return sortedStuds;
-         }
+        }
+        public static Student[] GroupStud (this IEnumerable<Student> students,int group) 
+        {
+        var grouped = students
+               .Where(x => x.GroupNumber == group).ToArray();
+        return grouped;
+        }
     }
 }
--- StudentGoups/Program.cs
+++ StudentGoups/Program.cs
@@ -11,14 +11,20 @@ namespace StudentGoups
     {
         public static void PrintAll(IEnumerable<Student> students,Predicate predicate)
         {
-            Console.WriteLine("==============");
+            Console.WriteLine("=============");
             var sortedStudents = predicate(students);
             foreach (var n in sortedStudents)
             {
                 Console.WriteLine(n.FirstName + "\r\n" + n.LastName + "\r\n" + n.FN + "\r\n" + n.Tel + "\r\n" + n.Email + "\r\n" + n.GroupNumber + "\r\n ==========");
             }
         }
-
+        public static void Print(IEnumerable<Student> students)
+        {
+            foreach (var n in students)
+            {
+                Console.WriteLine(n.FirstName + "\r\n" + n.LastName + "\r\n" + n.FN + "\r\n" + n.Tel + "\r\n" + n.Email + "\r\n" + n.GroupNumber + "\r\n ==========");
+            }
+        }
         public static IOrderedEnumerable<Student> FromGroup(IEnumerable<Student> students)
         {
             var studentsFromSecGrp = from student in students
@@ -52,38 +58,47 @@ namespace StudentGoups
                 new Student("Patkan" , "Bogomilski","Fakultet : 3406","08925623","Patkata@abv.bg",new List<string>() {"M : 2","AE : 2","NE : 2"},1 ),
                 new Student("Grigor" , "Dimitrov","Fakultet : 10","08724623","Grisho@keral.bg",new List<string>() {"M : 2","AE : 4"},6 ),
                 new Student("Stoyan" , "Murgavski","Fakultet : 7606","08593323","Tqnata@gmail.bg",new List<string>() {"M : 6","AE : 6","NE : 6"},2 ),
-                new Student("Frau" , "Koicheva","Fakultet : 10","08912693","Nadejdata@abv.bg",new List<string>() {"M : 4","AE : 5","NE : 3"},3 )
+                new Student("Frau" , "Koicheva","Fakultet : 10","08912693","Nadejdata@abv.bg",new List<string>() {"M : 4","AE : 5","NE : 3"},1 )
             };
 
-            PrintAll(students, FromGroup);
+             PrintAll(students, FromGroup);
 
-            PrintAll(students, WithAbv);
+             PrintAll(students, WithAbv);
 
-            PrintAll(students,SofiaNumber);
+             PrintAll(students,SofiaNumber);
 
-              var anonymousStud = from student in students
-                                 where String.Join(";  ", student.Marks).Contains("6")
-                                 select new { student.FirstName,student.Marks };
-              foreach (var n in anonymousStud)
-              {
-                  Console.WriteLine($"{n.FirstName}");
-              }
+             Print(students.GroupStud(1));
 
-            var studentsWithTwoMarks = students.ExtractWithTwoMarks();
+            var anonymousStud = from student in students
+                                  where String.Join(";  ", student.Marks).Contains("6")
+                                  select new { student.FirstName,student.Marks };
+               foreach (var n in anonymousStud)
+               {
+                   Console.WriteLine($"{n.FirstName}");
+               }
 
-            foreach (var student in studentsWithTwoMarks)
-            {
-                Console.WriteLine(student.FirstName);
-            }
-            
-            var marksFromFn = from student in students
-                              where student.FN.Contains("06")
-                              select new { student.Marks };
+             var studentsWithTwoMarks = students.ExtractWithTwoMarks();
 
-            foreach (var student in marksFromFn)
-            {
-                Console.WriteLine(String.Join(" ",student.Marks));
-            }  
+             foreach (var student in studentsWithTwoMarks)
+             {
+                 Console.WriteLine(student.FirstName);
+             }
+
+             var marksFromFn = from student in students
+                               where student.FN.Contains("06")
+                               select new { student.Marks };
+
+             foreach (var student in marksFromFn)
+             {
+                 Console.WriteLine(String.Join(" ",student.Marks));
+             }  
+
+             var groupedOne = students
+                .Where(x => x.GroupNumber == 1).ToArray();
+             foreach (var student in groupedOne)
+             {
+                 Console.WriteLine($"{student.FirstName} - {student.GroupNumber}");
+             }
         }
     }
 }

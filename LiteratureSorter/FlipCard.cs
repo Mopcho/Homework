f7ba2830e6001dbcf8563b2034fb3e899939e72d
Mopcho <valerkatapoower@abv.bg>
2020-05-30 22:20:27 +0300

    Finishing my console app for literature pieces learning

---
+++ LiteratureSorter/FlipCard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteratureSorter
+{
+   public class Engine
+   {
+        public Dictionary<string, string> Data = new Dictionary<string, string>();
+        private Random rnd = new Random();
+
+        public Engine(Dictionary<string, string> data)
+        {
+            this.Data = data;
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                int index = this.rnd.Next(0, this.Data.Count);
+
+                var pair = this.Data.ElementAt(index);
+
+                Console.WriteLine(pair.Key);
+
+                Console.ReadLine();
+
+                Console.WriteLine(pair.Value);
+
+                Console.ReadLine();
+
+                Console.Clear();
+
+            }
+        }
+   }
+}
+
---
+++ LiteratureSorter/IFlipCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteratureSorter
+{
+    public interface IFlipCard
+    {
+        List<string> CardInfo{get;}
+        void Test();
+    }
+}
